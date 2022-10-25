c523d987d031b640ab27a08e4176933a398143ce
cya98 <todaudcksgk@naver.com>
2022-10-25 10:55:44 +0900

    8-SaveCartItems

--- utils/Store.js
+++ utils/Store.js
@@ -1,9 +1,12 @@
 import { createContext, useReducer } from 'react'
+import Cookies from 'js-cookie'
 
 export const Store = createContext() // 스토어라는 객체를 만듬 createContext를 이용해서
 
 const initialState = {
-  cart: { cartItems: [] },
+  cart: Cookies.get('cart')
+    ? JSON.parse(Cookies.get('cart'))
+    : { cartItems: [] },
 }
 
 function reducer(state, action) {
@@ -18,6 +21,14 @@ function reducer(state, action) {
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem]
+      Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }))
+      return { ...state, cart: { ...state.cart, cartItems } }
+    } //쿠키는 스트링이라 stringify 로스트링으로 바꿔줌
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item.slug !== action.payload.slug //삭제했다..
+      )
+      Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }))
 
       return { ...state, cart: { ...state.cart, cartItems } }
     }
